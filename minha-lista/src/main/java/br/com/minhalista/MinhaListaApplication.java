bdb04505541bed3c8b3bdf94098b709a7ba88432
Luan Lima <lordellu@hotmail.com>
2023-05-03 22:40:40 -0300

    subindo back-end

---
+++ minha-lista/src/main/java/br/com/minhalista/MinhaListaApplication.java
@@ -0,0 +1,13 @@
+package br.com.minhalista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaListaApplication.class, args);
+	}
+
+}
---
+++ minha-lista/src/test/java/br/com/minhalista/minhalista/MinhaListaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.minhalista.minhalista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
